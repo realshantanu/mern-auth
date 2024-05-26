3f729b4c20265ab1cc6d55ca1c239e3bfa6a6f7c
Harshal141 <harshalmukundapatil@gmail.com>
2024-05-26 16:46:23 +0530

    new rental

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const connectDB = require('./config/db');
 const jwt = require('jsonwebtoken');
 
 const User = require('./models/User');
-
+const Rental = require('./models/Rental');
 
 app.use(express.json());
 app.use(cors());
@@ -17,6 +17,38 @@ connectDB();
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/api/newRental', async (req,res) => {
+    try{
+        console.log(req.body);
+        const Place = new Rental({
+            owner:req.body.owner,
+            title:req.body.title,
+            location:req.body.location,
+            area:req.body.area,
+            bedrooms:req.body.bedrooms,
+            bathrooms:req.body.bathrooms,
+            landmark:req.body.landmark,
+            rent:req.body.rent,
+            likes:0
+        })
+        await Place.save();
+        res.json({status:'ok'});
+    }catch(err){
+        console.log(err)
+        res.json({status:'error',error:"Server error"});
+    }
+})
+
+app.get('./api/getRentals', async(req,res) => {
+    try{
+        const blogs = await Rental.find({}).sort({createdAt: -1}).limit(10);
+        res.json({status:'ok',data:blogs});
+    }catch(err){
+        console.log(err)
+        res.json({status:'error',error:"Server error"});
+    }
 })
 
 app.post('/api/register', async (req, res) => {
