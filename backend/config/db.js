b1862e5af76f25508649479870a3de7982edcb3d
Harshal141 <harshalmukundapatil@gmail.com>
2024-05-26 16:13:26 +0530

    model auth

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,23 @@
-import mongoose from 'mongoose';
+const mongoose = require('mongoose'); 
+// const config = require('config');
+// const db = config.get('mongoURI');
+const dotenv = require('dotenv');
+dotenv.config();
+
+mongoose.set('strictQuery', false);
 
 const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
-  } catch (error) {
-    console.error(`Error: ${error.message}`);
-    process.exit(1);
-  }
-};
+    try {
+        await mongoose.connect(process.env.MONGOURI, {
+            useNewUrlParser: true,
+            // useCreateIndex: true
+        });
+        console.log('MongoDB Connected...');
+    } catch(err) {
+        console.error(err.message);
+        // Exit process with failure
+        process.exit(1);
+    }
+}
 
-export default connectDB;
+module.exports = connectDB;
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,69 @@
 const express = require('express')
+const dotenv = require('dotenv');
+const cors = require('cors')
+dotenv.config();
 const app = express()
 const port = 5000
+const connectDB = require('./config/db');
+const jwt = require('jsonwebtoken');
+
+const User = require('./models/User');
+
+
+app.use(express.json());
+app.use(cors());
+
+connectDB();
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+app.post('/api/register', async (req, res) => {
+
+    try{
+        const user = await User.findOne({email:req.body.email});
+        if(user){
+            return res.json({status:'error',error:"User already exists"});
+        }
+        const newUser = new User({
+            username:req.body.name,
+            number:req.body.number,
+            email:req.body.email,
+            password:req.body.password
+        });
+        await newUser.save();
+
+        res.json({status:'ok'});
+    }catch(err){
+        console.log(err)
+        res.json({status:'error',error:"Server error"});
+    }
+    console.log(req.body)
+});
+
+app.post('/api/login', async (req, res) => {
+
+    try{
+        const user = await User.findOne({
+            email:req.body.email,
+            password:req.body.password
+        })
+        if(!user){
+            return res.json({status:'error',error:"Invalid email or password"});
+        }
+        else{
+            const token = jwt.sign({
+                name: user.name,
+                email: user.email},process.env.JWTSECRET);
+            res.json({status:'ok', user:token});
+        }
+    }catch(err){
+        console.log(err)
+        res.json({status:'error',error:"Server error"});
+    }
+    console.log(req.body)
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
